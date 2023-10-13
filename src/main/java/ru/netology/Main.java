2ffaa76f1ca3eb13b41f19b9f5e5728ab0b2faf0
AZharick <bopohlabs@mail.ru>
2023-10-13 22:15:57 +0300

    Separated server methods

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,9 +1,10 @@
 package ru.netology;
 
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
      Server server = new Server();
      server.start();
-
   }
 }
